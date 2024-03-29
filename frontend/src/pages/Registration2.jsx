b9b96fe2fa7645dad8cc012ac207ce7380cc604f
abirrsahaa <abirsaha453@gmail.com>
2024-03-29 22:08:57 +0530

    changes made for a moemnbt

--- frontend/src/pages/Registration2.jsx
+++ frontend/src/pages/Registration2.jsx
@@ -16,11 +16,11 @@ const Registration2 = () => {
   // const [techtalkError, setTechtalkError] = useState(null);
   const options1 = [
     {
-      content:
-        "Day-1 (Introduction to Cybersecurity and Bug bounty)(BOOKED!!!)",
+      content: "Day-1 (Introduction to Cybersecurity and Bug bounty)",
     },
     {
-      content: "Day-1 (Understanding malware, and basic malware analysis)",
+      content:
+        "Day-1 (Understanding malware, and basic malware analysis)(BOOKED!!!)",
     },
   ];
   const options2 = [
@@ -185,7 +185,7 @@ const Registration2 = () => {
                         value={index}
                         checked={selected1 === index}
                         onChange={() => {
-                          index != 0 && setselected1(index);
+                          index != 1 && setselected1(index);
                         }}
                         className="bg-white text-white"
                       />
